b297b80aa5a1a31b569e596385fce757aa17c8c4
Luuk <luuk@voorhoede.nl>
2026-03-03 17:41:41 +0100

    Configure bucket policy to include SCW principal

--- src/storage/createBucket.ts
+++ src/storage/createBucket.ts
@@ -3,32 +3,39 @@ import {
   PutBucketPolicyCommand,
 } from "@aws-sdk/client-s3";
 import { client } from "./client";
+import { getInput } from "@actions/core";
 
 export async function createBucket(bucketName: string) {
+  const secretAccessKey = getInput("scw_secret_access_key", { required: true });
+
   const createBucketCommand = new CreateBucketCommand({ Bucket: bucketName });
   await client.send(createBucketCommand);
 
-  // const putBucketPolicyCommand = new PutBucketPolicyCommand({
-  //   Bucket: bucketName,
-  //   Policy: JSON.stringify({
-  //     Id: "Function Access",
-  //     Statement: [
-  //       {
-  //         Sid: "Public Read",
-  //         Effect: "Allow",
-  //         Principal: "*",
-  //         Action: ["s3:GetObject"],
-  //         Resource: [bucketName, `${bucketName}/*`],
-  //       },
-  //       {
-  //         Sid: "Public Read",
-  //         Effect: "Allow",
-  //         Principal: "*",
-  //         Action: ["*"],
-  //         Resource: [bucketName, `${bucketName}/*`],
-  //       }
-  //     ],
-  //   }),
-  // });
-  // await client.send(putBucketPolicyCommand);
+  const putBucketPolicyCommand = new PutBucketPolicyCommand({
+    Bucket: bucketName,
+    Policy: JSON.stringify({
+      Id: "Function Access",
+      Statement: [
+        {
+          Sid: "Public Read",
+          Effect: "Allow",
+          Principal: "*",
+          Action: ["s3:GetObject"],
+          Resource: [bucketName, `${bucketName}/*`],
+        },
+        {
+          Sid: "Scaleway secure statement",
+          Effect: "Allow",
+          Principal: {
+            SCW: [
+              `user_id:${secretAccessKey}`
+            ],
+          },
+          Action: "*",
+          Resource: [bucketName, `${bucketName}/*`],
+        },
+      ],
+    }),
+  });
+  await client.send(putBucketPolicyCommand);
 }
